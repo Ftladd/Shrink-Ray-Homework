55f21d954e0d64ce00980ffa56eec77fca8613cb
Ftladd <finlay.ladd@gmail.com>
2023-03-31 14:23:47 -0500

    -enitity relationships

---
+++ src/entities/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, Relation } from 'typeorm';
+import { Link } from './Link';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  userId: string;
+
+  @Column({ unique: true })
+  passwordHash: string;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+
+  @Column({ default: false })
+  isPro: boolean;
+
+  @OneToMany(() => Link, (link) => link.user)
+  links: Relation<Link>[];
+}
